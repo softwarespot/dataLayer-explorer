ccadf062189b874ba20e765928e721691ffd6a00
softwarespot <softwarespotapps@gmail.com>
2026-01-03 00:18:42 +0200

    Code tidying

--- src/formatters.js
+++ src/formatters.js
@@ -12,24 +12,23 @@ export function formatAsJSON(str) {
     const reParseJSON =
         // eslint-disable-next-line security/detect-unsafe-regex, sonarjs/regex-complexity
         /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g;
-
     return escapeHTML(str).replace(reParseJSON, (match) => {
         return `<span class="${getClassNameForJSON(match)}">${truncate(match, 256)}</span>`;
     });
 }
 
 function getClassNameForJSON(str) {
+    if (str === 'null') {
+        return 'json-null';
+    }
+    if (str === 'true' || str === 'false') {
+        return 'json-boolean';
+    }
     if (str.startsWith('"')) {
         if (str.endsWith(':')) {
             return 'json-key';
         }
         return 'json-string';
-    }
-    if (str === 'true' || str === 'false') {
-        return 'json-boolean';
-    }
-    if (str === 'null') {
-        return 'json-null';
     }
     return 'json-number';
 }
